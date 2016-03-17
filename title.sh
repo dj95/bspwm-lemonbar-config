1ffbe5e02c14cb9d68dd732a13f63e544fe9ad7b
dj95 <Daniel.Jankowski@rub.de>
2016-03-17 12:40:15 +0100

    updated bar config for bspwm 0.9.1

--- title.sh
+++ title.sh
@@ -1,29 +1,26 @@
 #!/bin/bash
 
-xtitle -s | while read line; do
+
+bspc subscribe | while read line; do
     output=""
-    
-    for desktop in $( bspc query -D ); do
-        ACTIVE_WS=false
-        
-        for line in "$( bspc query -T -d ${desktop} )"; do
-            if [[ "${line}" =~ ^(.*)0\,0\,0\,0\ M\ \-\ \*(.*)$ ]]; then # monocle mode
-                active=$( echo "${line}" | grep -oP '\w\ ([a-zA-Z]*)\ ([a-zA-Z]*)\ (0x[0-9A-F]*)\ (.*)\-\ \*' | grep -oP '0x[0-9A-F]{7}')
-                
-                # get titles
-                for win in $( bspc query -W -d ${desktop} ); do
-                    if [[ "$win" == "$active" ]]; then
+    focused_window_id=$( bspc query -N -n )
+    if [[ -z $focused_window_id ]]; then
+        output="%{F#000000}%{B#000000}%{T3}◤%{T-} "
+    else
+        if [[ $( bspc query -T -d | jshon -e layout ) = "\"tiled\"" ]]; then  
+            output="$( xtitle $focused_window_id | head -c 40 )%{F#000000}%{B#000000}%{T3}◤%{T-} "
+        else
+            for win in $( bspc query -N -d ); do
+                title=$( xtitle $win | head -c 40 )
+                if [[ $title != "" ]]; then
+                    if [[ $win = $focused_window_id ]]; then
                         output=$output"%{F#000000}%{B#905D5D5D}%{T3}◤%{T-}%{F#BDBDBD} "$( xtitle $win | head -c 40 )" %{F#905D5D5D}%{B#000000}%{T3}◤%{T-}"
                     else
                         output=$output"%{F#000000}%{B#000000}%{T3}◤%{T-}%{F#BDBDBD} "$( xtitle $win | head -c 40 )" %{F#000000}%{B#000000}%{T3}◤%{T-}"
                     fi
-                done
-            elif [[ "${line}" =~ ^(.*)0\,0\,0\,0\ T\ \-\ \*(.*)$ ]]; then
-                active=$( echo "${line}" | grep -oP '\w\ ([a-zA-Z]*)\ ([a-zA-Z]*)\ (0x[0-9A-F]*)\ (.*)\-\ \*' | grep -oP '0x[0-9A-F]{7}')
-                output="$( xtitle $active | head -c 40 )%{F#000000}%{B#000000}%{T3}◤%{T-} "
-            fi
-        done
-    done
-
+                fi
+            done
+        fi
+    fi
     echo "T$output"
 done
